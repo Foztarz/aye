a410a8cc25e395ea9f57ad0d33fb9c803db82178
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2016-05-27 16:01:04 +0000

    Sending frames as JPEG to server at 40fps

--- client.py
+++ client.py
@@ -3,13 +3,18 @@ from picamera.array import PiRGBArray
 import cv2
 import time
 import io
+import socket
+import struct
+
+consumer_socket = socket.socket()
+consumer_socket.connect(('172.24.1.1', 8123))
+consumer = consumer_socket.makefile('wb')
 
 camera = PiCamera()
 resolution = (320, 240)
 camera.resolution = resolution
 camera.framerate = 60
-raw_capture = PiRGBArray(camera, size=resolution)
-#raw_capture = io.BytesIO()
+raw_capture = io.BytesIO()
 
 smoothing = 0.9
 average_fps = 0
@@ -17,18 +22,25 @@ average_fps = 0
 time.sleep(1)
 start = time.time()
 
-for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+try:
+    for frame in camera.capture_continuous(raw_capture, format="jpeg", use_video_port=True):
 
-    cv2.imshow("Frame", frame.array)
-    cv2.waitKey(1) & 0xFF
+        consumer.write(struct.pack('<L', raw_capture.tell()))
+        consumer.flush()
+        raw_capture.seek(0)
 
-    raw_capture.seek(0)
-    raw_capture.truncate(0)
+        consumer.write(raw_capture.read())
+        raw_capture.seek(0)
+        raw_capture.truncate(0)
 
-    time_taken = time.time() - start
-    current_fps = 1./time_taken
-    average_fps = average_fps * smoothing + current_fps * (1 - smoothing)        
-    print average_fps
+        time_taken = time.time() - start
+        current_fps = 1./time_taken
+        average_fps = average_fps * smoothing + current_fps * (1 - smoothing)        
+        print average_fps
 
-    start = time.time()
+        start = time.time()
+
+finally:
+    consumer.close()
+    consumer_socket.close()
 
