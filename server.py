437fc5940f2bedccdf8ac4f39c91f82e7fb966d1
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2016-06-03 18:08:32 +0100

    Timestamp synchronization works although it hangs occasionally

--- server.py
+++ server.py
@@ -30,7 +30,10 @@ def consume(connection_file):
 
 def synchronized(producer_address, timestamp, queues):
     synchronized = True
-    for other_producer_address, queue in queue.items():
+    for other_producer_address, queue in queues.items():
+        if len(queue) == 0: 
+            synchronized = False
+            break
         other_timestamp = queue[0][1]
         if producer_address is not other_producer_address:
             synchronized = synchronized and (abs(timestamp - other_timestamp) < SYNCHRONIZED_THRESHOLD_MS)
@@ -41,11 +44,12 @@ def show(queues):
     for producer_address, queue in queues.items():
         image = queue[0][0]
         cv2.imshow(producer_address, image)
-    cv2.waitKey(1) & 0xFF                 
+        cv2.waitKey(1) & 0xFF                 
 
 def pop(queues):
-    for producer_address, queue in queues.items():
-        queues[producer_address] = queue[1:]
+    for producer_address in queues.keys():
+        current = queues[producer_address]
+        queues[producer_address] = current[1:]
 
 server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', 8123))
@@ -72,28 +76,35 @@ try:
                 file = connection.makefile('rb')
                 producers.append(file)
                 file_to_address[file] = address[0]
+                queues = {}
             else:
                 producer_address = file_to_address[to_read]
                 image, timestamp = consume(to_read)
-
 
                 # IMPORTANT we are assuming no packet loss - otherwise it is possible that the received timestamp is later than the earliest received of other producers and it is still the earliest for this producer
 
                 # add image, timestamp to producer queue 
 
-                if not queues.has_key(producer_address):
-                    # this is the earliest timestamp for producer 
-                    # if it is close to the other two, display and pop the first in the queue
-                    if synchronized(producer_address, timestamp, queues):
-                        show(queues)
-                        pop(queues)
-                    else:
-                        # else, discard the other two (as we are not going to receive an earlier timestamp for producer due to TCP)
-                        for other_producer_address, queue in queues.items():
-                            if other_producer_address is not producer_address:
-                                queues[other_producer_address] = queue[1:]
+                queues.setdefault(producer_address, []).append((image, timestamp))
+
+                image, timestamp = queues[producer_address][0]
+                # this is the earliest timestamp for producer 
+                # if it is close to the other two, display and pop the first in the queue
+                if synchronized(producer_address, timestamp, queues):
+                    show(queues)
+                    pop(queues)
                 else:
-                    queues[producer_address] += [(image, timestamp)]
+                    # else, discard the ones that are not close enough 
+                    for other_producer_address, queue in queues.items():
+                        if other_producer_address is producer_address:
+                            continue
+                        discard_until = -1
+                        for index, (_, other_timestamp) in enumerate(queue):
+                            if abs(timestamp - other_timestamp) > SYNCHRONIZED_THRESHOLD_MS:
+                                discard_until = index
+                        queues[other_producer_address] = queue[discard_until+1:]
+
+                print map(len, queues.values())
 
 finally:
     for producer in producers:
