f48751e1d4a113b16756e6a541426d99c3998ce9
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2016-06-20 10:10:27 +0100

    Fixing bugs with the client start scripts. Now they work

--- start_clients.sh
+++ start_clients.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python aye/restart_client &
-ssh 172.24.1.91 python aye/restart_client.sh &
-ssh 172.24.1.97 python aye/restart_client.sh &
+aye/restart_client.sh &
+ssh 172.24.1.91 aye/restart_client.sh &
+ssh 172.24.1.97 aye/restart_client.sh &
 bash
