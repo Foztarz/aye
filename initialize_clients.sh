49afabf9f0b67a39e6b981bc20f453358ee93731
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2016-06-20 12:16:50 +0100

    Another fix for orchestration commands

--- initialize_clients.sh
+++ initialize_clients.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-aye/stop_clients.sh
+~/aye/stop_clients.sh
 
-aye/start_client.sh &
-ssh 172.24.1.91 aye/start_client.sh &
-ssh 172.24.1.97 aye/start_client.sh &
+~/aye/start_client.sh &
+ssh 172.24.1.91 ~/aye/start_client.sh &
+ssh 172.24.1.97 ~/aye/start_client.sh &
+bash
--- stop_clients.sh
+++ stop_clients.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aye/stop_client.sh 
-ssh 172.24.1.91 aye/stop_client.sh
-ssh 172.24.1.97 aye/stop_client.sh
+~/aye/stop_client.sh 
+ssh 172.24.1.91 ~/aye/stop_client.sh
+ssh 172.24.1.97 ~/aye/stop_client.sh
 
 
