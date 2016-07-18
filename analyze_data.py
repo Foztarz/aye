8321bfbb64690116c9728295e61b74d33b5cdddc
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2016-07-18 15:58:20 +0100

    Refactoring and adding code to save all the zenith images instead of displaying them

--- analyze_data.py
+++ analyze_data.py
@@ -50,123 +50,21 @@ def parse_orientation(image_file_path):
 def get_aye_pol_image(image_name):
     return parse_orientation(image_name), parse_millis(image_name), cv2.imread(image_name)
 
-def head(queues, name):
-    if not queues.has_key(name):
-        return None, None
-    queue = queues[name]
-    if queue is None or len(queue) < 1:
-        return None, None
-    
-    return queue[0]
-
-def synchronized_test(t1, t2):
-    return t1 is not None and t2 is not None and abs(t1 - t2) < SYNCHRONIZED_THRESHOLD_MS
-
-def synchronized(t1, t2, t3):
-    return synchronized_test(t1,t2) and synchronized_test(t2,t3) and synchronized_test(t1,t3)
-
-def pop(queues):
-    for producer_name in queues.keys():
-        current = queues[producer_name]
-        queues[producer_name] = current[1:]
-
-def synchronize(directories0n45n90):
-    sequences = map(get_files_in_directory, directories0n45n90)
-
-    map(list.sort, sequences)
-
-    queues = {}
-    positions = [0] * len(sequences)
-    while True:
-        for index, sequence in enumerate(sequences):
-            position = positions[index]
-            if len(sequence) <= position:
-                return
-            image_name = sequence[position]
-            positions[index] = position + 1
-            producer_name, timestamp, image = get_aye_pol_image(image_name)
-
-            queues.setdefault(producer_name, []).append((image, timestamp))
-
-            first90image, first90timestamp = head(queues, 90)
-            first45image, first45timestamp = head(queues, 45)
-            first0image, first0timestamp = head(queues, 0)
-
-            if first90image is None or first45image is None or first0image is None:
-                pass
-            elif synchronized(first90timestamp, first45timestamp, first0timestamp):
-                yield [(0, first0image, first0timestamp), (45, first45image, first45timestamp), (90, first90image, first90timestamp)]
-                pop(queues)
-            else:
-                heads = [(90, first90timestamp),(45, first45timestamp),(0, first0timestamp)]
-                heads = filter(lambda h: h[1] is not None, heads)
-                heads.sort(key=itemgetter(1))
-                latest_timestamp = heads[-1][1]
-                late_heads = filter(lambda h: not synchronized_test(h[1], latest_timestamp), heads)
-                for late_key, _ in late_heads:
-                    queues[late_key] = queues[late_key][1:]
-
-            print(positions)
-            #print zip(queues.keys(), map(len, queues.values()))
-            if max(map(len, queues.values())) > 500:
-                print("Queues are too big, emptying them to prevent memory freeze")
-                queues = {}
-
-def get_latest_data():
-    sequence_directories = get_sequence_directories()
-    millis_sequence_categories = zip(map(parse_millis, sequence_directories), sequence_directories)
-    millis_sequence_categories.sort(reverse=True)
-    orientation_to_directory = {}
-    for millis, sequence_directory in millis_sequence_categories:
-        orientation = parse_orientation(sequence_directory)
-        orientation_to_directory[orientation] = sequence_directory
-        if len(orientation_to_directory.keys()) == 3:
-            return synchronize(orientation_to_directory.values())
-    return None
-
-data = []
-
-def interactive():
-    global data
-    cv2.namedWindow("0")
-    cv2.namedWindow("45")
-    cv2.namedWindow("90")
-
-    for data in get_latest_data():
-        current_data = data
-        map(lambda d: cv2.imshow(str(d[0]), d[1]), data)
-        display(data)
-        cv2.waitKey()
-
-gray0, gray45, gray90, stokesI, stokesQ, stokesU, polInt, polDoLP, polAoP = None, None, None, None, None, None, None, None, None
-
 def gray(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 def small(image):
-    return cv2.resize(image, (480, 360))
-
-def display(data):
-    global gray0, gray45, gray90, stokesI, stokesQ, stokesU, polInt, polDoLP, polAoP
-    gray0 = gray(data[0][1])
-    gray45 = gray(data[1][1])
-    gray90 = gray(data[2][1])
-    stokesI, stokesQ, stokesU, polInt, polDoLP, polAoP = stokes.getStokes(gray0, gray45, gray90)
-    cv2.imshow('stokes-i', aye_utils.normalized_uint8(stokesI, 500))
-    cv2.imshow('stokes-q', aye_utils.normalized_uint8(stokesQ, 255))
-    cv2.imshow('stokes-u', aye_utils.normalized_uint8(stokesU, 255))
-    cv2.imshow('linear-intensity', aye_utils.normalized_uint8(polInt, 255))
-    cv2.imshow('linear-degree', polDoLP)
-    cv2.imshow('angle', cv2.cvtColor(cv2.merge(stokes.angle_hsv(polAoP)), cv2.COLOR_HSV2BGR))
-
-    cv2.imshow('hsv', cv2.cvtColor(cv2.merge(stokes.toHSV(polInt, polDoLP, polAoP)), cv2.COLOR_HSV2BGR))
+    global show_subset
+    if show_subset:
+        return cv2.resize(image, (720, 640))
+    else:
+        return cv2.resize(image, (480, 360))
 
 def save_stokes(stokesI, stokesQ, stokesU):
     dir = 'stokes'
     if not os.path.isdir(dir):
         os.mkdir(dir)
 
-    cv2.imwrite('/'.join([dir, 'stokes-i.png']), stokesI)
     cv2.imwrite('/'.join([dir, 'stokes-q.png']), stokesQ)
     cv2.imwrite('/'.join([dir, 'stokes-u.png']), stokesU)
 
@@ -198,34 +96,16 @@ class PixelInfo:
 
 def sunny(image, sun_at_degrees):
     middle = np.array(image.shape[:2]) / 2
-    sun_orbit_radius = np.min(middle) - 50
-    sun_position = point_on_circle(np.deg2rad(sun_at_degrees), sun_orbit_radius) + middle
-    return cv2.circle(image, tuple(sun_position.astype(int)[::-1]), 20, (255, 255, 0), -1)
+    sun_orbit_radius = 100
+    sun_direction = point_on_circle(np.deg2rad(sun_at_degrees), sun_orbit_radius) + middle
+    return cv2.arrowedLine(image, tuple(middle[::-1]), tuple(sun_direction.astype(int)[::-1]), (0, 255, 255), 2)
 
-
-def display_stokes(rotation_images):
-    rotation, images = rotation_images
-    stokesI, stokesQ, stokesU, polInt, polDoLP, polAoP = stokes.getStokes(images[0], images[1], images[2])
-
-    normalized_stokesI = aye_utils.normalized_uint8(stokesI, 500)
-    normalized_stokesQ = aye_utils.normalized_uint8(stokesQ, 255)
-    normalized_stokesU = aye_utils.normalized_uint8(stokesU, 255)
-    save_stokes(normalized_stokesI, normalized_stokesQ, normalized_stokesU)
-
-    cv2.imshow(suffixed('0'), images[0])
-    cv2.imshow(suffixed('45'), images[1])
-    cv2.imshow(suffixed('90'), images[2])
-    cv2.imshow(suffixed('linear-degree'), polDoLP)
-    cv2.imshow(suffixed('stokes-q'), normalized_stokesQ)
-    cv2.imshow(suffixed('stokes-u'), normalized_stokesU)
-    angle_image = cv2.cvtColor(cv2.merge(stokes.angle_hsv(polAoP)), cv2.COLOR_HSV2BGR) 
-    cv2.imshow(suffixed('angle'), angle_image)
-
-    angle_in_degrees = stokes.angle_to_hue(polAoP)
+def with_evectors_and_sun(pol_angle_radians, rotation):
     downsample = 15
-    polAoP_downsampled = small(polAoP[::downsample, ::downsample])
-    angle_image_downsampled = cv2.cvtColor(cv2.merge(stokes.angle_hsv(polAoP_downsampled)), cv2.COLOR_HSV2BGR) 
-    angle_in_degrees_downsampled = stokes.angle_to_hue(polAoP_downsampled)
+    angle_in_degrees = stokes.angle_to_hue(pol_angle_radians)
+    pol_angle_radians_downsampled = small(pol_angle_radians[::downsample, ::downsample])
+    angle_image_downsampled = cv2.cvtColor(cv2.merge(stokes.angle_hsv(pol_angle_radians_downsampled)), cv2.COLOR_HSV2BGR) 
+    angle_in_degrees_downsampled = stokes.angle_to_hue(pol_angle_radians_downsampled)
 
     width = angle_in_degrees.shape[0]
     height = angle_in_degrees.shape[1]
@@ -240,11 +120,42 @@ def display_stokes(rotation_images):
     system_orientation_sun_starting_angle = 110
     angle_image_with_lines_and_sun = sunny(angle_image_with_lines, system_orientation_sun_starting_angle + rotation)
 
-    cv2.imshow(suffixed('angle-with-lines'), angle_image_with_lines)
+    return angle_image_with_lines_and_sun
 
-    pixel_info = PixelInfo([('angle', angle_in_degrees_downsampled), ('pol-degree', polDoLP), ('angle-raw', polAoP_downsampled)]) 
-    cv2.setMouseCallback(suffixed('angle-with-lines'), pixel_info.output)
-    cv2.setMouseCallback(suffixed('linear-degree'), pixel_info.output)
+def display_labeled_images(labeled_images):
+    global show_subset
+    labels_subset = ['0', 'linear-degree', 'angle-evectors']
+    for label, image in labeled_images:
+        if not show_subset or label in labels_subset:
+            cv2.imshow(suffixed(label), image)
+
+def display_stokes(time_rotation_images, save_directory = None):
+    (time, rotation), images = time_rotation_images
+
+    stokesI, stokesQ, stokesU, polInt, polDoLP, polAoP = stokes.getStokes(images[0], images[1], images[2])
+
+    normalized_stokesI = aye_utils.normalized_uint8(stokesI, 500)
+    normalized_stokesQ = aye_utils.normalized_uint8(stokesQ, 255)
+    normalized_stokesU = aye_utils.normalized_uint8(stokesU, 255)
+    angle_image = cv2.cvtColor(cv2.merge(stokes.angle_hsv(polAoP)), cv2.COLOR_HSV2BGR) 
+    angle_evectors = with_evectors_and_sun(polAoP, rotation)
+
+    normalized_degree = aye_utils.normalized_uint8(polDoLP, 1)
+    normalized_angle = aye_utils.normalized_uint8(angle_image, 1)
+    normalized_angle_evectors = aye_utils.normalized_uint8(angle_evectors, 1)
+
+
+    labels = ['0', '45', '90', 'linear-degree', 'stokes-q', 'stokes-u', 'angle', 'angle-evectors']
+    labeled_images = zip(labels, [images[0], images[1], images[2], normalized_degree, normalized_stokesQ, normalized_stokesU, normalized_angle, normalized_angle_evectors])
+
+    if save_directory:
+        save_labeled_images(time, rotation, labeled_images, save_directory)
+    else:
+        update_control_view(time, rotation)
+        display_labeled_images(labeled_images)
+        pixel_info = PixelInfo([('angle', angle_in_degrees_downsampled), ('pol-degree', polDoLP), ('angle-raw', polAoP_downsampled)]) 
+        cv2.setMouseCallback(suffixed('angle-with-lines'), pixel_info.output)
+        cv2.setMouseCallback(suffixed('linear-degree'), pixel_info.output)
 
 def parse_zenith_time(file_path):
     time_text = os.path.split(file_path)[-1].split('-')[-1]
@@ -265,15 +176,17 @@ def update_control_view(minutes, rotation):
 def parse_rotation(sevilla_zenith_image_path):
     return int(os.path.split(sevilla_zenith_image_path)[-1].split('-')[4])
 
-def visualize_sevilla_zenith(directory, use_raw=True, first=None, last=None):
+def visualize_sevilla_zenith(directory, use_raw=True, first=None, last=None, save_directory = False):
     zenith_times = filter(lambda f: 'azim' in f, get_files_in_directory(directory))
-    zenith_times = map(itemgetter(1), sorted(zip(map(parse_zenith_time, zenith_times), zenith_times)))
+    parsed_zenith_times = map(parse_zenith_time, zenith_times)
+    zenith_times = map(itemgetter(1), sorted(zip(parsed_zenith_times, zenith_times)))
     zenith_time_rotation = []
     time_points = len(list(zenith_times))
     print('%d time points' % time_points)
     max_data_points = 0
     count = -1
-    for zenith_time in  zenith_times:
+
+    for zenith_time in zenith_times:
         count = count + 1
         if first and first > count:
             zenith_time_rotation.append([])
@@ -305,24 +218,44 @@ def visualize_sevilla_zenith(directory, use_raw=True, first=None, last=None):
         small_gray0, small_gray45, small_gray90 = map(lambda i: map(small, map(gray, i)), [images0, images45, images90])
         assert map(parse_rotation, rotation_sorted_pol_0) == map(parse_rotation, rotation_sorted_pol_45)
         assert map(parse_rotation, rotation_sorted_pol_0) == map(parse_rotation, rotation_sorted_pol_90)
-        zipped_rotation_images = zip(map(parse_rotation, rotation_sorted_pol_0), zip(small_gray0, small_gray45, small_gray90))
+        rotations = map(parse_rotation, rotation_sorted_pol_0)
+        time_rotations = [(parse_zenith_time(zenith_time), rotation) for rotation in rotations]
+        zipped_rotation_images = zip(time_rotations, zip(small_gray0, small_gray45, small_gray90))
         zenith_time_rotation.append(zipped_rotation_images)
 
         if last and last <= count:
             break
 
-    display_trackbar = lambda _: display(cv2.getTrackbarPos('Time Index', suffixed('control')), cv2.getTrackbarPos('Rotation Index', suffixed('control')))
-    display = lambda time_index, rotation_index: update_control_view(parse_zenith_time(zenith_times[time_index]), zenith_time_rotation[time_index][rotation_index][0]) or display_stokes(zenith_time_rotation[time_index][rotation_index])
+    if save_directory:
+        print("Saving to %s" % save_directory)
+        save(zenith_time_rotation, save_directory)
+    else:
+        display_trackbar = lambda _: display(cv2.getTrackbarPos('Time Index', suffixed('control')), cv2.getTrackbarPos('Rotation Index', suffixed('control')))
+        display = lambda time_index, rotation_index: display_stokes(zenith_time_rotation[time_index][rotation_index])
 
-    cv2.namedWindow(suffixed('control'))
-    cv2.createTrackbar('Time Index', suffixed('control'), 0, time_points, display_trackbar)
-    cv2.createTrackbar('Rotation Index', suffixed('control'), 0, max_data_points, display_trackbar)
+        cv2.namedWindow(suffixed('control'))
+        cv2.createTrackbar('Time Index', suffixed('control'), 0, time_points, display_trackbar)
+        cv2.createTrackbar('Rotation Index', suffixed('control'), 0, max_data_points, display_trackbar)
 
-    while (1):
-        k = cv2.waitKey() & 0xFF
+        while (1):
+            k = cv2.waitKey() & 0xFF
 
-        if k == 27:
-            break
+            if k == 27:
+                break
+
+def save(time_rotation_images, save_directory):
+    for rotation_images in time_rotation_images:
+        for images in rotation_images:
+            display_stokes(images, save_directory)
+
+def save_labeled_images(time, rotation, labeled_images, directory):
+    hour_minutes = minutes_to_hour_minutes(time)
+    time_directory = "%s/%s" % (directory, hour_minutes)
+    if not os.path.isdir(time_directory):
+        os.makedirs(time_directory)
+
+    for label, image in labeled_images:
+        cv2.imwrite('/'.join([time_directory, '%s-%s-%s.png' % (hour_minutes, rotation, label)]), image)
 
 def file_to_png(file_path):
     image = cv2.imread(file_path)
@@ -341,4 +274,5 @@ def raw_to_image(file_path, out=None, ext='.png', to_small=False, to_gray=False)
         cv2.imwrite(os.path.splitext(file_path)[0] + ext, image)
 
 if __name__ == '__main__':
-    visualize_sevilla_zenith(sys.argv[1], False, first=11, last=20)
+    show_subset = False
+    visualize_sevilla_zenith(sys.argv[1], False, save_directory='aye-sevilla-zenith-stokes-analysis')
--- stokes.py
+++ stokes.py
@@ -9,7 +9,7 @@ def getStokes(gray0, gray45, gray90):
     #calculate Stokes parameters
     stokesI=gray0_d+gray90_d+.1 #.1 added here to prevent division by zero later on
     stokesQ=gray0_d-gray90_d
-    stokesU=2*gray45_d-gray0_d-gray90_d + 1
+    stokesU=2*gray45_d-gray0_d-gray90_d + .1
     #calculate polarization parameters
     polInt=np.sqrt(1+np.square(.1+stokesQ)+np.square(.1+stokesU)) #Linear Polarization Intensity
 
@@ -28,11 +28,14 @@ def toHSV(polInt, polDoLP, polAoP):
     V = np.uint8(polInt)
     return [H,S,V]
 
-def angle_hsv(angle):
+def angle_hsv(angle, degree=None):
     H = np.float32(angle_to_hue(angle))
     S = np.ones(angle.shape, 'float32')
-    V = np.ones(angle.shape, 'float32')
-
+    
+    if degree is None:
+        V = np.ones(angle.shape, 'float32')
+    else:
+        V = np.float32(degree) / np.amax(degree)
     
     return [H,S,V]
     
